0a3557141eaa0db363738b1d44c097780151070d
NasibaAkbarova <nasibaakbarova190@gmail.com>
2025-11-02 10:54:19 +0500

    navbae da ozgarishlar qoshildii

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from 'react'
+import { lazy } from 'react'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 import { Route,Routes } from 'react-router-dom'
